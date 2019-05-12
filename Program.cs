19bd46d407506edb05217cb9a26ab94a863b329b
ju$tin <justin@garofolo.net>
2019-05-12 05:18:05 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_Sharp {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
